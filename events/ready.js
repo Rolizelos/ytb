a0b45c6dc67bdcaec74017df29600d7a68872c5e
Glitch (stark-code-bos-altyapi) <none>
2020-05-03 14:33:57 +0000

    👖🏵 Checkpoint ./events/ready.js:10803881/2508 ./ayarlar.json:10803881/28 ./events/guildMemberAdd.js:10803881/71 ./events/guildMemberRemove.js:10803881/71 ./bot.js:10803881/176 ./package.json:10803881/109

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,51 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 
 var prefix = ayarlar.prefix;
 
-
 module.exports = client => {
-  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  
+  console.log(
+    ` [${moment().format(
+      "YYYY-MM-DD HH:mm:ss"
+    )}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("dnd");
   //idle = boşta
   //dnd = rahatsız etmeyin
   //online = çevrimiçi
-  console.log(`                                                                                                                                                                     `)
-  client.user.setActivity(`SELAĞM`, { type: "PLAYİNG"});
+  client.user.setActivity("Bla Bla", {
+    type: "LISTENING"
   //LISTENING = DİNLİYOR
   //WATCHING = İZLİYOR
   //PLAYING = OYNUYOR 
-  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  }); 
+  console.log(
+    ` [${moment().format(
+      "YYYY-MM-DD HH:mm:ss"
+    )}] Oyun ismi ayarlandı!`
+  );
+  console.log(
+    `CODE Hub | [${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Şu an ` +
+      client.channels.size +
+      ` adet kanala, ` +
+      client.guilds.size +
+      ` adet sunucuya ve ` +
+      client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() +
+      ` kullanıcıya hizmet veriliyor!`
+  );
 };
+
+
+
+
+
+
+
+
